c191a95c1eb0ca051f522d162aea0b1b24564cf8
DenisKonn <140009429+DenisKonn@users.noreply.github.com>
2023-11-19 19:02:13 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,67 +1,33 @@
-def generate_random_data():
-
-    for item in data:
-    return data
-
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    main()
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-
-def main():
-import randomdef generate_random_data():
-
-
-if __name__ == "__main__":
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-def main():
-
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    for item in data:
-    main()
-    return datapublic class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-
-    }
-}
-        int[] data = new int[10];
-
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
-        }
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {}
         console.log("Random Number: " + item);
     const data = [];
 
-    });
-    }
     for (let i = 0; i < 10; i++) {
+
+    return data;
+function main() {
+    }
+
+}
+}
         data.push(Math.floor(Math.random() * 100) + 1);
     data.forEach(item => {
-main();
-function generateRandomData() {
-    return data;
-
-
-function main() {
+    });
     const data = generateRandomData();
-}if __name__ == "__main__":
-def generate_random_data():
-    for item in data:
-        print(f"Random Number: {item}")
-import random
+function generateRandomData() {
+main();}
+    return data;
+    }
+    for (let i = 0; i < 10; i++) {
 
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
 
-def main():
 
-    return data
+    const data = [];
+function main() {
+}
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+    const data = generateRandomData();
+function generateRandomData() {
+main();
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
